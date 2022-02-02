6d1df5418c9405ad16b75170db3b4fb6e95c38e6
chankruze <chankruze@gmail.com>
2022-02-03 00:14:58 +0530

    subscription demo with apollo 3
    Signed-off-by: chankruze <chankruze@gmail.com>

--- index.js
+++ index.js
@@ -14,6 +14,7 @@ const { makeExecutableSchema } = require('@graphql-tools/schema');
 const gql = require('graphql-tag');
 const express = require('express');
 const http = require('http');
+const { PubSub } = require('graphql-subscriptions');
 const { generateId, cryptPassword, comparePassword, getAge } = require('./utils');
 
 // schema
@@ -64,34 +65,32 @@ const typeDefs = gql`
     }
 `;
 
-// const NEW_USER = 'NEW_USER';
+const NEW_USER = 'NEW_USER';
 
 // resolvers
 const resolvers = {
-    // Subscription: {
-    //     newUser: {
-    //         subscribe: (parent, args, { pubsub }) => {
-    //             return pubsub.asyncIterator(NEW_USER);
-    //         }
-    //     }
-    // },
+    Subscription: {
+        newUser: {
+            subscribe: (_, __, { pubsub }) => pubsub.asyncIterator(NEW_USER)
+        }
+    },
     User: {
         age: parent => getAge(parent.dob)
     },
     Query: {
-        hello: (parent, { name }, context, info) => `Hello ${name}!`,
-        user: async (parent, { userInfo }, context) => ({
+        hello: (_, { name }) => `Hello ${name}!`,
+        user: async (_, { userInfo }) => ({
             ...userInfo,
             id: generateId(),
             password: await cryptPassword(userInfo.password),
         })
     },
     Mutation: {
-        login: async (parent, { userInfo: { password } }, context) => {
+        login: async (_, { userInfo: { password } }) => {
             const hashedPass = await cryptPassword(password);
             return await comparePassword(password, hashedPass);;
         },
-        register: async (parent, { userInfo }, context) => {
+        register: async (_, { userInfo }, { pubsub }) => {
             const { password } = userInfo;
             const newUser = {
                 ...userInfo,
@@ -99,8 +98,7 @@ const resolvers = {
                 password: await cryptPassword(password),
             }
 
-            // pubsub.pubsub(NEW_USER, { newUser });
-            console.log(context);
+            pubsub.publish(NEW_USER, { newUser });
 
             return {
                 errors: [],
@@ -116,10 +114,12 @@ const resolvers = {
     const app = express();
     const httpServer = http.createServer(app);
     const schema = makeExecutableSchema({ typeDefs, resolvers });
+    const pubsub = new PubSub();
 
     // Same ApolloServer initialization as before, plus the drain plugin.
     const server = new ApolloServer({
         schema,
+        context: ({ req, res }) => ({ req, res, pubsub }),
         plugins: [ApolloServerPluginDrainHttpServer({ httpServer }), {
             async serverWillStart() {
                 return {
@@ -145,9 +145,16 @@ const resolvers = {
             webSocket,
             context
         ) {
+            console.log('Connected!');
             // If an object is returned here, it will be passed as the `context`
             // argument to your subscription resolvers.
-        }
+            return {
+                pubsub
+            }
+        },
+        onDisconnect(webSocket, context) {
+            console.log('Disconnected!')
+        },
     }, {
         // This is the `httpServer` we created in a previous step.
         server: httpServer,
@@ -163,7 +170,6 @@ const resolvers = {
         // By default, apollo-server hosts its GraphQL endpoint at the
         // server root. However, *other* Apollo Server packages host it at
         // /graphql. Optionally provide this to match apollo-server.
-        path: '/',
     });
 
     // Modified server startup
